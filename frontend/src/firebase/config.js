5b161df3632269027dfa717204b3a534fc0ba511
Diego Tapia R <dftapiaru@falabella.cl>
2021-09-22 21:59:55 -0300

    deployment

---
+++ frontend/src/firebase/config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+export default {
+  apiKey: 'AIzaSyBZQ2u3e4GEDDwMZlgEDV0PEQrXnN5rPRY',
+  authDomain: 'rootchile-boolean.firebaseapp.com',
+  projectId: 'rootchile-boolean',
+  storageBucket: 'rootchile-boolean.appspot.com',
+  messagingSenderId: '968981922090',
+  appId: '1:968981922090:web:911b6440224f862f781e53'
+}
